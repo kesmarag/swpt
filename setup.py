2623326de39fee79e8413d85b534f34c7e13ecb9
Costas Smaragdakis <kesmarag@iacm.forth.gr>
2019-10-19 11:44:42 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='kesmarag-swpt',
+      version='0.0.1',
+      description='Stationary Wavelet Packet Transform',
+      author='Costas Smaragdakis',
+      author_email='kesmarag@gmail.com',
+      url='https://github.com/kesmarag/swpt',
+      packages=['kesmarag.swpt'],
+      package_dir={'kesmarag.swpt': './'},
+      install_requires=['PyWavelets>=0.5.2', 'numpy'], )
---
+++ swpt.py
@@ -0,0 +1,174 @@
+import numpy as np
+import seaborn
+import pywt
+import re
+import matplotlib.pyplot as plt
+plt.style.use(['seaborn'])
+# from pywt._thresholding import hard, soft
+
+
+class SWPT(object):
+
+  def __init__(self, wavelet='db4', max_level=3):
+    self._wavelet = wavelet
+    self._max_level = max_level
+    self._coeff_dict = {}
+    self._entropy_dict = {}
+
+  def decompose(self, signal, entropy='shannon'):
+    pth = ['']
+    self._coeff_dict[''] = np.squeeze(signal)
+    for l in range(self._max_level):
+      pth_new = []
+      for p in pth:
+        coeff = pywt.swt(
+            self._coeff_dict[p],
+            wavelet=self._wavelet,
+            level=self._max_level - len(p),
+            start_level=len(p))
+        p_run = p
+        for i, C in enumerate(coeff[::-1]):
+          self._coeff_dict[p_run + 'A'] = C[0]
+          self._coeff_dict[p_run + 'D'] = C[1]
+          self._entropy_dict[p_run + 'A'] = 0.0
+          self._entropy_dict[p_run + 'D'] = 0.0
+          for c in C[0]:
+            self._entropy_dict[p_run + 'A'] += self._get_entropy(c, signal, entropy)
+          self._entropy_dict[p_run + 'A'] = self._entropy_dict[p_run + 'A'] / 2 ** (len(p_run) + 2.0)
+          for c in C[1]:
+            self._entropy_dict[p_run + 'D'] += self._get_entropy(c, signal, entropy)
+          self._entropy_dict[p_run + 'D'] = self._entropy_dict[p_run + 'D'] / 2 ** (len(p_run) + 2.0)
+          if i < len(coeff) - 1 and len(p_run) < self._max_level - 1:
+            pth_new.append(p_run + 'D')
+            p_run = p_run + 'A'
+      pth = list(pth_new)
+
+  def get_level(self, level, order='freq', thresholding=None, threshold=None):
+    assert order in ['natural', 'freq']
+    r = []
+    result_coeffs = []
+    result_energies = []
+    for k in self._coeff_dict:
+      if len(k) == level:
+        r.append(k)
+    if order == 'freq':
+      graycode_order = self._get_graycode_order(level)
+      for p in graycode_order:
+        if p in r:
+          result_coeffs.append(self._coeff_dict[p])
+    else:
+      print('The natural order is not supported yet.')
+      exit(1)
+    # apply the thressholding
+    if thresholding in ['hard', 'soft']:
+      if isinstance(threshold, (int, float)):
+        if thresholding == 'hard':
+          result_coeffs = hard(result_coeffs, threshold)
+        else:
+          result_coeffs = soft(result_coeffs, threshold)
+      else:
+        print('Threshold must be an integer or float number')
+        exit(1)
+    return result_coeffs
+
+  def get_coefficient_vector(self, name):
+    return self._coeff_dict[name]
+
+  def _get_graycode_order(self, level, x='A', y='D'):
+    graycode_order = [x, y]
+    for i in range(level - 1):
+      graycode_order = [x + path for path in graycode_order] + \
+                       [y + path for path in graycode_order[::-1]]
+    return graycode_order
+
+  def _get_entropy(self, c, s, entropy):
+    if entropy == 'shannon':
+      return -np.log(c ** 2 / np.linalg.norm(s, ord=2) ** 2 ) * c ** 2 / np.linalg.norm(s, ord=2) ** 2
+    elif entropy == "log-entropy":
+      return np.log(c ** 2 / np.linalg.norm(s, ord=2) ** 2 )
+
+
+  def best_basis(self):
+    best_entropy = {}
+    best_entropy_fr = {}
+    levels = self._max_level
+    cur_keys = [k for k in self._coeff_dict if len(k)==levels]
+    lev_ord = self._get_graycode_order(levels)
+    frqs = np.linspace(0.0 + 0.5/(2 * len(lev_ord)), 0.5 - 0.5/(2 * len(lev_ord)), len(lev_ord))
+    for i in range(len(lev_ord)):
+      best_entropy_fr[lev_ord[i]] = frqs[i]
+    for k in cur_keys:
+      best_entropy[k] = self._entropy_dict[k]
+    for lev in range(levels-1, 0, -1):
+      cur_keys = [k for k in self._coeff_dict if len(k)==lev]
+      lev_ord = self._get_graycode_order(lev)
+      frqs = np.linspace(0.0 + 0.5/(2 * len(lev_ord)), 0.5 - 0.5/(2 * len(lev_ord)), len(lev_ord))
+      for i in range(len(lev_ord)):
+        best_entropy_fr[lev_ord[i]] = frqs[i]
+      for k in cur_keys:
+        if self._entropy_dict[k] < self._entropy_dict[k+'A'] + self._entropy_dict[k+'D']:
+          best_entropy[k] = self._entropy_dict[k]
+          rx = re.compile(k+'.')
+          del_key = [q for q in best_entropy if rx.search(q)]
+          for d in del_key:
+            del best_entropy[d]
+            del best_entropy_fr[d]
+          # print('prunning')
+        else:
+          best_entropy[k] = self._entropy_dict[k+'A'] + self._entropy_dict[k+'D']
+    for lev in range(1, levels):
+      cur_keys = [k for k in self._coeff_dict if len(k)==lev]
+      for k in cur_keys:
+        if k+'A' in best_entropy.keys():
+          del best_entropy[k]
+          del best_entropy_fr[k]
+    total_entropy = sum(best_entropy.values())
+    sorted_x = sorted(best_entropy_fr.items(), key=lambda kv: kv[1])
+    best_tree = []
+    for x in sorted_x:
+      best_tree.append([x[0], x[1], best_entropy[x[0]]])
+    return best_tree
+
+  def plot_best_basis(self):
+    best_tree = self.best_basis()
+    coeff = []
+    p = []
+    m = 0
+    for c in best_tree:
+      if len(c[0]) > m:
+        m = len(c[0])
+    # print(m)
+    for c in best_tree:
+      l = m - len(c[0]) + 1
+      p.append(c[2])
+      # print(l)
+      for ell in range(l):
+      # coeff.append(np.log(np.abs(self._coeff_dict[c[0]])/max(np.abs(self._coeff_dict[c[0]]))+1))
+        coeff.append(np.abs(self._coeff_dict[c[0]]))
+    coeff = np.array(coeff)
+    print(best_tree)
+    plt.plot(p, '*')
+    plt.figure()
+    # plt.show()
+    plt.pcolor(coeff)
+    plt.show()
+
+
+
+
+if __name__ == '__main__':
+  act = np.load('/home/kesmarag/Github/pscs-earthquakes/retreat_2019/act.npz')
+  # act = np.load('/home/kesmarag/Github/pscs-earthquakes/retreat_2019/sim1.npz')
+  x = act['arr_0']
+  swpt = SWPT(max_level=7, wavelet='db4')
+  # swpt.decompose(x, 'log-entropy')
+  swpt.decompose(x, 'shannon')
+  # wp4 = swpt.get_level(4)
+  # print(swpt._entropy_dict)
+  # best = swpt.best_basis()
+  swpt.plot_best_basis()
+  # plt.plot(swpt.get_coefficient_vector('AAAAAD'))
+  # plt.plot(swpt.get_coefficient_vector('AAAAAAD'))
+  # plt.plot(swpt.get_coefficient_vector('AAAADDA'))
+  # plt.show()
+  # print(best)
